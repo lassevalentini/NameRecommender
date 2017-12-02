6383a0502c6b133381e0a1b6a94a93b2aa5cf65e
Lasse Valentini Jensen <lasse.valentini@gmail.com>
2017-12-02 16:47:51 +0100

    Restructure, multiple possible models
    Added interactive mode

--- model.py
+++ model.py
@@ -1,71 +1,136 @@
 from gensim.models import Doc2Vec
+from gensim.models.phrases import Phrases
 from gensim.models.doc2vec import TaggedDocument
-from argparse import ArgumentParser
-import json
 import pickle
 import os
-import re
+from nltk import ngrams
+import random
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Masking
+from keras.layers import LSTM
+from keras.optimizers import RMSprop
+import numpy as np
 
-parser = ArgumentParser()
-parser.add_argument("-t", "--train", action="store_true", help="Train a new prod2vec model", default=False)
-parser.add_argument("-m", "--model-path", help="Path to the model pickle", default='model.pickle')
-parser.add_argument("-p", "--positive-names", help="", default='positive-names.txt')
-parser.add_argument("-n", "--negative-names", help="", default='negative-names.txt')
-parser.add_argument("--match-regex", help="", default='^[a-væøå]+$')
-parser.add_argument("--ignore-regex", help="", default='([wxyzq]|aa|ee|uu)')
-parser.add_argument("names", help="Text file with all names, each name on a separate line")
 
-args = parser.parse_args()
+class NameRecommendModel:
+	def __init__(self, opts, all_names, positive_names, negative_names):
+		self.opts = opts
+		self.all_names = all_names
+		self.negative_names = negative_names
+		self.positive_names = positive_names
+		print(len(all_names), len(positive_names), len(negative_names))
 
-match_regex = re.compile(args.match_regex, re.IGNORECASE)
-ignore_regex = re.compile(args.ignore_regex, re.IGNORECASE)
 
-if args.train or not os.path.exists(args.model_path):
-	# Train model
-	# documents = TaggedLineDocument(args.names)
-	documents = []
-	with open(args.names, "r") as names:
-		i = 0
-		for name in names:
-			if (not match_regex.search(name)) or ignore_regex.search(name):
-			# if match_regex.search(name):
-				# print("Skipping {0}".format(name[:-1]))
-				# print(match_regex.search(name), ignore_regex.search(name))
-				# print()
-				pass
+	def train(self):
+		pass
+
+
+	def make_recommendation(self):
+		pass
+
+
+class Dec2VecModel(NameRecommendModel):
+	def make_bigrams(self, name):
+		r = ["".join(list(gram)) for gram in ngrams(name, self.opts.ngrams)]
+		return r
+		# return bigram_transformer[name]
+
+
+	def train(self):
+		# bigram_transformer = Phrases((list(name) for name in self.all_names), threshold=1)
+		if self.opts.train or not os.path.exists(self.opts.model_path):
+			# Train model
+			documents = [TaggedDocument(self.self.make_bigrams(list(name)), [i]) for i,name in enumerate(self.all_names)]
+			self.model = Doc2Vec(size=25, window=2, min_count=5, workers=4, iter=20)
+			self.model.build_vocab(documents)
+			self.model.train(documents, total_examples=self.model.corpus_count, epochs=self.model.iter)
+			with open(self.opts.model_path, 'wb') as model_pickle:
+				pickle.dump(self.model, model_pickle)
+		else:
+			with open(self.opts.model_path, 'rb') as model_pickle:
+				self.model = pickle.load(model_pickle)
+
+
+	def vectorize(self, name):
+		return self.model.infer_vector(self.make_bigrams(name))
+
+
+	def make_recommendation(self):
+		possible_suggestions = []
+
+		for name in self.positive_names:
+			print(name, self.make_bigrams(name))
+			inferred_vector = self.vectorize(name)
+			sims = self.model.docvecs.most_similar([inferred_vector], topn=100)
+			print("{0} top {1}:".format("".join(name), len(sims)))
+			n_printed = 0
+			for sim in [(self.all_names[i],s) for i,s in sims]:
+				if n_printed >= 5:
+					break
+				if not sim[0] in self.negative_names and not sim[0] in self.positive_names:
+					print("\t", sim)
+					n_printed+=1
+					possible_suggestions.append(sim)
+
+		negative_name_vectors=[self.vectorize(name) for name in self.negative_names]
+		positive_name_vectors=[self.vectorize(name) for name in self.positive_names]
+		
+		sims = self.model.docvecs.most_similar(positive=positive_name_vectors, negative=negative_name_vectors, topn=10)
+
+		print("Top 10 of all:")
+		for sim in [(self.all_names[i],s) for i,s in sims]:
+			print("\t", sim)
+			possible_suggestions.append(sim)
+
+		sum_similarity = sum((sim[1] for sim in possible_suggestions))
+		choice = random.uniform(0, sum_similarity)
+		comul = 0
+		for name,sim in possible_suggestions:
+			comul+=sim
+			if comul > choice:
+				return name
+		return possible_suggestions[-1][0]
+
+
+
+class LstmModel(NameRecommendModel):
+	def train(self):
+		chars = set()
+		maxlen = 1
+		for name in self.all_names:
+			maxlen = max(len(name), maxlen)
+			for char in name:
+				chars.add(char)
+		chars_map = {c: i for i,c in enumerate(chars)}
+
+		self.model = Sequential()
+		self.model.add(Masking(mask_value=0., input_shape=(maxlen, len(chars))))
+		self.model.add(LSTM(128))
+		self.model.add(Dense(1, activation='sigmoid'))
+		self.model.add(Activation('softmax'))
+
+		optimizer = RMSprop(lr=0.01)
+		self.model.compile(loss='binary_crossentropy', optimizer=optimizer)
+
+		train_names = [name for name in self.positive_names if name in self.all_names] + [name for name in self.negative_names if name in self.all_names]
+
+		x = np.zeros((len(train_names), maxlen, len(chars)), dtype=np.bool)
+		y = np.zeros((len(train_names), len(chars)), dtype=np.bool)
+
+		for i,name in enumerate(train_names):
+			if name in self.positive_names:
+				y[i] = 1
+			elif name in self.negative_names:
+				y[i] = 0
 			else:
-				documents.append(TaggedDocument(list(name.lower().replace('\n', '')), [i]))
-				i += 1
+				y[i] = 0.5
+				print("Name not in positive or negative: {0}".format(name))
 
-	print("{0} names after filtering".format(len(documents)))
-	model = Doc2Vec(size=50, window=2, min_count=5, workers=4, iter=20)
-	model.build_vocab(documents)
-	model.train(documents, total_examples=model.corpus_count, epochs=model.iter)
-	with open(args.model_path, 'wb') as model_pickle:
-		pickle.dump(model, model_pickle)
-else:
-	with open(args.model_path, 'rb') as model_pickle:
-		model = pickle.load(model_pickle)
+			for j,c in enumerate(name):
+				char_index = chars_map[c]
+				x[i, j, char_index] = 1
 
-# positive_names = TaggedLineDocument(args.positive_names)
-positive_names = []
-with open(args.positive_names, "r") as names:
-	for i, name in enumerate(names):
-		positive_names.append(list(name.lower().replace('\n', '')))
+		self.model.fit(x, y, batch_size=128, epochs=1000)
 
-negative_names = []
-with open(args.negative_names, "r") as names:
-	for i, name in enumerate(names):
-		negative_names.append(model.infer_vector(list(name.lower().replace('\n', ''))))
-
-for name in positive_names:
-	inferred_vector = model.infer_vector(name)
-	sims = model.docvecs.most_similar([inferred_vector], topn=5)
-	print("{0} top {1}:".format("".join(name), len(sims)))
-	for sim in [("".join(documents[i].words),s) for i,s in sims]:
-		print("\t", sim)
-
-sims = model.docvecs.most_similar(positive=[model.infer_vector(name) for name in positive_names], negative=negative_names, topn=10)
-print("Top 10 of all:")
-for sim in [("".join(documents[i].words),s) for i,s in sims]:
-	print("\t", sim)
+	def make_recommendation(self):
+		pass
---
+++ name_recommender.py
@@ -0,0 +1,79 @@
+from argparse import ArgumentParser
+from model import Dec2VecModel, LstmModel
+import re
+
+parser = ArgumentParser()
+parser.add_argument("-t", "--train", action="store_true", help="Train a new prod2vec model", default=False)
+parser.add_argument("-m", "--model-path", help="Path to the model pickle", default='model.pickle')
+parser.add_argument("-p", "--positive-names", help="", default='positive-names.txt')
+parser.add_argument("-n", "--negative-names", help="", default='negative-names.txt')
+parser.add_argument("--match-regex", help="", default='^[a-væøå]+$')
+parser.add_argument("--ignore-regex", help="", default='([wxyzq]|aa|ee|uu|ii|oo)')
+parser.add_argument("--ngrams", help="", default=2)
+parser.add_argument("names", help="Text file with all names, each name on a separate line")
+
+args = parser.parse_args()
+
+
+def ask_name(name):
+	decision = input("{0}? (y/n)".format(name))
+	return decision == "y"
+
+
+def add_to_negative(name):
+	negative_names_file.write("\n"+name)
+	negative_names.add(name)
+
+
+def add_to_positive(name):
+	positive_names_file.write("\n"+name)
+	positive_names.add(name)
+
+
+def clean_name(name):
+	return name.lower().replace('\n', '')
+
+
+match_regex = re.compile(args.match_regex, re.IGNORECASE)
+ignore_regex = re.compile(args.ignore_regex, re.IGNORECASE)
+
+all_names = set()
+with open(args.names, "r") as names:
+	for name in names:
+		if (not match_regex.search(name)) or ignore_regex.search(name):
+		# if match_regex.search(name):
+			# print("Skipping {0}".format(name[:-1]))
+			# print(match_regex.search(name), ignore_regex.search(name))
+			# print()
+			pass
+		else:
+			name = clean_name(name)
+			all_names.add(name)
+
+	print("{0} names after filtering".format(len(all_names)))
+
+# positive_names = TaggedLineDocument(args.positive_names)
+positive_names = set()
+positive_names_file = open(args.positive_names, "a+")
+positive_names_file.seek(0)
+for name in positive_names_file:
+	positive_names.add(clean_name(name))
+
+negative_names = set()
+negative_names_file = open(args.negative_names, "a+")
+negative_names_file.seek(0)
+for name in negative_names_file:
+	negative_names.add(clean_name(name))
+
+
+model = LstmModel(args, all_names, positive_names, negative_names)
+model.train()
+
+while True:
+	suggestion = model.make_recommendation()
+	decision = ask_name(suggestion)
+	if decision:
+		add_to_positive(suggestion)
+	else:
+		add_to_negative(suggestion)
+		
