abcecfcf185f5cc1bdd8b395c5dd97254499d319
Lasse Valentini Jensen <lasse.valentini@gmail.com>
2021-01-13 16:19:03 +0100

    Fix name_recommender only running if called directly

--- name_recommender/name_recommender.py
+++ name_recommender/name_recommender.py
@@ -42,106 +42,108 @@ def read_name_file(name_file_name, name_scores):
     for line in name_file:
         score, name = line.split(" ", 1)
         name = clean_name(name)
-        if not name in name_scores and name in all_names:
+        if name not in name_scores and name in all_names:
             name_scores[name] = int(score)
     return name_file
 
 
-parser = ArgumentParser()
-parser.add_argument(
-    "-t",
-    "--train",
-    action="store_true",
-    help="Train a new dov2vec model (only used by Dec2VecModel)",
-    default=False,
-)
-parser.add_argument(
-    "-m",
-    "--model-path",
-    help="Path to the model pickle (only used by Dec2VecModel)",
-    default="model.pickle",
-)
-parser.add_argument(
-    "-p",
-    "--positive-names",
-    help="File with positive ratings (score > 2)",
-    default="positive-names.txt",
-)
-parser.add_argument(
-    "-n",
-    "--negative-names",
-    help="File with positive ratings (score <= 2)",
-    default="negative-names.txt",
-)
-parser.add_argument(
-    "-b",
-    "--balance-classes",
-    help="Should positive and negative names be balanced for training (possible values: upsample, downsample, none)",
-    default="upsample",
-)
-parser.add_argument("-s", "--sample-factor", help="", type=int, default=10)
-parser.add_argument(
-    "--match-regex",
-    help="Names must match regex to be considered",
-    default="^[a-væøå]{3,10}$",
-)
-parser.add_argument(
-    "--ignore-regex",
-    help="Names must not match regex to be considered",
-    default="([wxyzq]|aa|ee|uu|ii|oo)",
-)
-parser.add_argument("--ngrams", help="Size of n-grams used for Dec2VecModel", default=2)
-parser.add_argument(
-    "names", help="Text file with all names, each name on a separate line"
-)
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument(
+        "-t",
+        "--train",
+        action="store_true",
+        help="Train a new dov2vec model (only used by Dec2VecModel)",
+        default=False,
+    )
+    parser.add_argument(
+        "-m",
+        "--model-path",
+        help="Path to the model pickle (only used by Dec2VecModel)",
+        default="model.pickle",
+    )
+    parser.add_argument(
+        "-p",
+        "--positive-names",
+        help="File with positive ratings (score > 2)",
+        default="positive-names.txt",
+    )
+    parser.add_argument(
+        "-n",
+        "--negative-names",
+        help="File with positive ratings (score <= 2)",
+        default="negative-names.txt",
+    )
+    parser.add_argument(
+        "-b",
+        "--balance-classes",
+        help="Should positive and negative names be balanced for training (possible values: upsample, downsample, none)",
+        default="upsample",
+    )
+    parser.add_argument("-s", "--sample-factor", help="", type=int, default=10)
+    parser.add_argument(
+        "--match-regex",
+        help="Names must match regex to be considered",
+        default="^[a-væøå]{3,10}$",
+    )
+    parser.add_argument(
+        "--ignore-regex",
+        help="Names must not match regex to be considered",
+        default="([wxyzq]|aa|ee|uu|ii|oo)",
+    )
+    parser.add_argument(
+        "--ngrams", help="Size of n-grams used for Dec2VecModel", default=2
+    )
+    parser.add_argument(
+        "names", help="Text file with all names, each name on a separate line"
+    )
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
+    match_regex = re.compile(args.match_regex, re.IGNORECASE)
+    ignore_regex = re.compile(args.ignore_regex, re.IGNORECASE)
 
-match_regex = re.compile(args.match_regex, re.IGNORECASE)
-ignore_regex = re.compile(args.ignore_regex, re.IGNORECASE)
+    all_names = set()
+    with open(args.names, "r", encoding="utf-8") as names:
+        for name in names:
+            if (not match_regex.search(name)) or ignore_regex.search(name):
+                pass
+            else:
+                name = clean_name(name)
+                all_names.add(name)
 
-all_names = set()
-with open(args.names, "r", encoding="utf-8") as names:
-    for name in names:
-        if (not match_regex.search(name)) or ignore_regex.search(name):
-            pass
-        else:
-            name = clean_name(name)
-            all_names.add(name)
+        print("{0} names after filtering".format(len(all_names)))
 
-    print("{0} names after filtering".format(len(all_names)))
+    name_scores = {}
+    positive_names_file = read_name_file(args.positive_names, name_scores)
+    negative_names_file = read_name_file(args.negative_names, name_scores)
 
-name_scores = {}
-positive_names_file = read_name_file(args.positive_names, name_scores)
-negative_names_file = read_name_file(args.negative_names, name_scores)
+    print("Using LstmModel - for other models see name_recommender.py")
+    model = LstmModel(args, all_names, name_scores)
+    ### Alternative models ###
+    # model = SimpleRnnModel(args, all_names, name_scores)
+    # model = Dec2VecModel(args, all_names, name_scores)
+    # model = AutoencoderModel(args, all_names, name_scores)
+    model.train()
 
-print("Using LstmModel - for other models see name_recommender.py")
-model = LstmModel(args, all_names, name_scores)
-### Alternative models ###
-# model = SimpleRnnModel(args, all_names, name_scores)
-# model = Dec2VecModel(args, all_names, name_scores)
-# model = AutoencoderModel(args, all_names, name_scores)
-model.train()
+    try:
+        suggestions_made = 0
+        while True:
+            suggestions_made += 1
+            suggestion = model.make_recommendation()
 
-try:
-    suggestions_made = 0
-    while True:
-        suggestions_made += 1
-        suggestion = model.make_recommendation()
+            if not suggestion:
+                model.retrain()
+                continue
 
-        if not suggestion:
-            model.retrain()
-            continue
+            score = ask_name(suggestion)
+            if score > 2:
+                add_to_positive(suggestion, score)
+            else:
+                add_to_negative(suggestion, score)
 
-        score = ask_name(suggestion)
-        if score > 2:
-            add_to_positive(suggestion, score)
-        else:
-            add_to_negative(suggestion, score)
+            if suggestions_made % 10 == 0:
+                model.retrain()
 
-        if suggestions_made % 10 == 0:
-            model.retrain()
-
-except KeyboardInterrupt:
-    pass
+    except KeyboardInterrupt:
+        pass
